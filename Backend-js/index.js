005095eb6f114b8f040028e4e2f9b60ccdbc4190
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2025-02-06 17:50:25 +0530

    added message

--- Backend-js/index.js
+++ Backend-js/index.js
@@ -21,6 +21,7 @@ app.use(helmet()); //Secure HTTP Headers
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 Minutes
   max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests please try again later",
   skip: (req, res) => req.ip === "192.168.29.1", // Bypass rate limit for your IP
 });
 
