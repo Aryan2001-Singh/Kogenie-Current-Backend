d213d99f8bea57498e9ad85aa48771a207129a01
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2025-02-06 17:19:29 +0530

    increased rate limiter

--- Backend-js/index.js
+++ Backend-js/index.js
@@ -21,7 +21,7 @@ app.use(helmet()); //Secure HTTP Headers
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, //15 Minutes 
-  max: 100 //Limit each IP to 100 requests per windowMs
+  max: 1000 //Limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 app.use(
