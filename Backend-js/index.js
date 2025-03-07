229d8ff6dbe54e51fd3c1a40353a2f6e4931daf4
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2025-03-07 17:32:12 +0530

    testing

--- Backend-js/index.js
+++ Backend-js/index.js
@@ -43,8 +43,14 @@ app.use(
 );
 
 // ✅ Add this line to handle preflight requests properly
-app.options("*", (req, res) => {
-  res.sendStatus(200);
+// app.options("*", (req, res) => {
+//   res.sendStatus(200);
+// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 app.use("/api/ads", adRoutes);
 app.use((req, res, next) => {
